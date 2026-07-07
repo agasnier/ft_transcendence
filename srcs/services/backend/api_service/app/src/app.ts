971215a4cdd603caca93a2e5976e79a1662011c5
Alex <alex@MacBook-Pro-de-Alex.local>
2026-07-07 15:08:23 +0200

    feat(infra): api_service not connected to db

--- srcs/services/backend/api_service/app/src/app.ts
+++ srcs/services/backend/api_service/app/src/app.ts
@@ -11,7 +11,7 @@ export function buildApp(): FastifyInstance {
   })
 
   // all module added must be register here
-  app.register(databaseRoutes, { prefix: '/auth' })
+  app.register(databaseRoutes, { prefix: '/api' })
 
   return app
 }
--- srcs/services/backend/api_service/app/src/modules/hello/hello.service.ts
+++ srcs/services/backend/api_service/app/src/modules/hello/hello.service.ts
@@ -1,7 +1,8 @@
-import { db } from '../../db/index.js'
-import { users } from '../../db/schema.js'
+// import { db } from '../../db/index.js'
+// import { users } from '../../db/schema.js'
 
 export async function checkDatabase(): Promise<{ message: string }> {
-  const rows = await db.select().from(users)
-  return { message: `Backend connecté à la DB — ${rows.length}` }
+  // const rows = await db.select().from(users)
+  // return { message: `Backend connecté à la DB — ${rows.length}` }
+  return { message: `hello` }
 }
