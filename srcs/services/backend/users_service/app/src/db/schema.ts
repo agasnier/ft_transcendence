841573472f0eade06e5d18a47087239610680a25
Alex <alex@macbookpro.home>
2026-07-10 15:51:11 +0200

    refactor(infra): adding role to users db

--- srcs/services/backend/users_service/app/src/db/schema.ts
+++ srcs/services/backend/users_service/app/src/db/schema.ts
@@ -1,9 +1,10 @@
-import { mysqlTable, int, varchar } from 'drizzle-orm/mysql-core'
+import { mysqlTable, int, varchar, mysqlEnum } from 'drizzle-orm/mysql-core'
 
 export const users = mysqlTable('users', {
   id: int('id').autoincrement().primaryKey(),
   pseudo: varchar('pseudo', { length: 255 }).notNull().unique(),
   password: varchar('password', { length: 255 }).notNull(),
+  role: mysqlEnum('role', ['admin', 'user']).notNull().default('user'),
 })
 
 export type User = typeof users.$inferSelect
