c68f90cc6cd34535f43897eb1de917c22b63871c
Alex <alex@macbookpro.home>
2026-07-12 22:39:17 +0200

    feat(back): connection when inscrition

--- srcs/services/backend/users_service/app/src/modules/users/users.controller.ts
+++ srcs/services/backend/users_service/app/src/modules/users/users.controller.ts
@@ -1,5 +1,6 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
 import { getAllUsers, getUserById, createUser, updateUser, deleteUser } from './users.service.js'
+import { createAccessToken, createRefreshToken } from '../auth/auth.service.js'
 
 export async function listUsersController(request: FastifyRequest, reply: FastifyReply): Promise<void> {
   try {
@@ -37,6 +38,15 @@ export async function createUserController(
   try {
     const { pseudo, password } = request.body
     const user = await createUser(pseudo, password)
+
+    // from logging to log the user during inscriptions
+    const accessToken = createAccessToken({ id: user.id, pseudo: user.pseudo, role: 'user' })
+    const refreshToken = await createRefreshToken(user.id)
+
+    reply
+      .setCookie('access_token', accessToken, { httpOnly: true, secure: true, sameSite: 'strict', path: '/' })
+      .setCookie('refresh_token', refreshToken, { httpOnly: true, secure: true, sameSite: 'strict', path: '/users' })
+
     await reply.status(201).send(user)
   } catch (err) {
     request.log.error(err)
