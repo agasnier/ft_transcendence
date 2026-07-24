b9d70bf4bbc0bd520ebb303e53f1efdc878790b9
jimbow <jb.odone@hotmail.fr>
2026-07-24 13:57:31 +0200

    feat(back): add listFriends search func

--- srcs/services/backend/users_service/app/src/modules/friends/friends.controller.ts
+++ srcs/services/backend/users_service/app/src/modules/friends/friends.controller.ts
@@ -1,5 +1,5 @@
 import type { FastifyRequest, FastifyReply } from 'fastify'
-import { sendFriendRequest, acceptFriendRequest } from './friends.service.js'
+import { sendFriendRequest, acceptFriendRequest, listFriends } from './friends.service.js'
 
 export async function sendFriendRequestController(req: FastifyRequest, reply:FastifyReply) {
 	if (!req.user) {
@@ -24,4 +24,14 @@ export async function acceptFriendRequestController(req: FastifyRequest, reply:F
 	const { userId } = req.params as { userId: string }
 	await acceptFriendRequest(Number(userId), req.user.id)
 	return reply.send({ message: 'Friend request accepted' })
+}
+
+export async function listFriendsController(req: FastifyRequest, reply:FastifyReply) {
+	if (!req.user) {
+		await reply.code(401).send({ message: 'Not authentificated' })
+		return
+	}
+	const { search } = req.query as { search?: string }
+	const friendsList = await listFriends(req.user.id, search)
+	return reply.send(friendsList)
 }
--- srcs/services/backend/users_service/app/src/modules/friends/friends.route.ts
+++ srcs/services/backend/users_service/app/src/modules/friends/friends.route.ts
@@ -1,9 +1,10 @@
 import type { FastifyInstance } from 'fastify'
-import { sendFriendRequestController, acceptFriendRequestController } from './friends.controller.js'
-import { sendFriendRequestSchema, acceptFriendRequestSchema } from './friends.schema.js'
+import { sendFriendRequestController, acceptFriendRequestController, listFriendsController } from './friends.controller.js'
+import { sendFriendRequestSchema, acceptFriendRequestSchema, listFriendsSchema } from './friends.schema.js'
 import { userAuthHook } from '../auth/auth.controller.js'
 
 export async function friendsRoutes(app: FastifyInstance) {
 	app.post('/:userId', { schema: sendFriendRequestSchema, preHandler: [userAuthHook] }, sendFriendRequestController)
 	app.patch('/:userId/accept', { schema: acceptFriendRequestSchema, preHandler: [userAuthHook] }, acceptFriendRequestController)
+	app.get('/', { schema: listFriendsSchema, preHandler: [userAuthHook] }, listFriendsController)
 }
