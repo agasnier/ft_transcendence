b9cedef441be44118a05001e115f2224b11310fd
Alex <alex@macbookpro.home>
2026-07-28 11:59:02 +0200

    feat(back): vault config for hash users srv

--- srcs/services/vault/tools/entrypoint.dev.sh
+++ srcs/services/vault/tools/entrypoint.dev.sh
@@ -27,9 +27,10 @@ fi
 	vault write -f transit/keys/passwords 2>/dev/null || true
 	vault write -f transit/keys/jwt type=ecdsa-p256 2>/dev/null || true
 
-	# Transit: HMAC key for api_service API keys only (default key type)
+	# enable transit
 	vault secrets enable transit 2>/dev/null || true
 	vault write -f transit/keys/api-keys 2>/dev/null || true
+	vault write -f transit/keys/passwords 2>/dev/null || true
 
 	# enable secrets database for dynamic credentials
 	vault secrets enable database
